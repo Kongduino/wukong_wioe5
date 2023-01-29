d85dc2dece52b9f0a2fa8367e48966a42754b714
Kongduino <dbvic@me.com>
2023-01-29 12:38:43 +0800

    Update

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 A short demo showing how to use a [SeeedStudio Wio-e5](https://wiki.seeedstudio.com/Grove_LoRa_E5_New_Version/) with a RPi pico and an [Elecfreaks Wukong 2040](https://www.elecfreaks.com/elecfreaks-wukong2040-breakout-board-for-raspberry-pi-pico.html). It uses my fork of the [SSD1306 library for MP](https://github.com/Kongduino/ssd1306_mp).
 
+See this [Twitter thread](https://twitter.com/Kongduino/status/1615538626636701698) for more information.
+
+## BOM
+
+* **Wukong 2040** by Elecfreaks
+* **Raspberry Pico**
+* **Wio-E5** by SeeedStudio
+* **SSD1306 OLED** – by... well, many places :-) Get one with the proper pinout: `GND Vcc SCL SDA`
+* **18650** battery
+
 ![demo](demo.gif)
 
 
--- wukong_wioe5.py
+++ wukong_wioe5.py
@@ -1,3 +1,7 @@
+# See https://twitter.com/Kongduino/status/1615538626636701698
+# Basic LoRa Receiver. It doesn't do much more than sending a msg
+# and display incoming messages, but that's a good place to start.
+
 from machine import UART
 import binascii, time, re, neopixel, machine, json
 from ssd1306 import SSD1306_I2C
@@ -12,16 +16,18 @@ oled = SSD1306_I2C(128, 64, i2c)
 print('UART Wio-E5 Serial')
 
 def sendString(txt):
-  np[1] = (0, 255, 0)
+  # Sends a string encoded in hexadecimal.
+  np[1] = (0, 255, 0) # Neopixel #1 = Green
   np.write()
   s = binascii.hexlify(txt)
   txt=s.decode('utf-8')
   result = uartSerialRxMonitor(f'AT+TEST=TXLRPKT, {txt}')
-  np[1] = (0, 0, 0)
+  np[1] = (0, 0, 0) # Neopixel #1 = OFF
   np.write()
   return result
 
 def uartSerialRxMonitor(command):
+  # Sends a command, terminated by CRLF, and returns the output from the firmware.
   print(command)
   uart.write(command + '\r\n')
   time.sleep(0.5)
@@ -32,26 +38,36 @@ def uartSerialRxMonitor(command):
   res=recv.decode('utf-8')
   return res.strip()
 
-def configLoRa(freq = 868.125, SF=12, BW=125, txPR=8, rxPR = 8,
-               txPower = 22, crc = "OFF", iq = "OFF", net = "OFF"):
+def configLoRa(freq = 868.125, SF=12, BW=125, txPR=8, rxPR = 8, txPower = 22, crc = "OFF", iq = "OFF", net = "OFF"):
+  # There are a couple of issues with the AT firmware:
+  # 1. The naming `TEST` is misleading, and seems to make LoRa a 3rd-class citizen.
+  #    It is after all the transport layer used by LoRaWAN, and not an afterthought.
+  # 2. Coding Rate, aka C/R, is not configurable. This is not ideal, to say the least.
+  #    All AT firmwares I know allow for C/R setup. No reason not to allow it.
+  # 3. Bandwidth setting: while LoRaWAN only has a subset of LoRa's BW, 125, 250, 500,
+  #    LoRa's full set should be enabled:
+  #    {7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250, 500}
+  #    This is very important when you work with LoRa and need the longest range possible
   uartSerialRxMonitor(f'AT+TEST=RFCFG,{freq},SF{SF},{BW},{txPR},{rxPR},{txPower},{crc},{iq},{net}')
 
 def handleIncoming():
-  np[0] = (0, 255, 0)
-  np[1] = (0, 0, 0)
+  np[0] = (0, 255, 0) # Neopixel #0 = Green
+  np[1] = (0, 0, 0) # Neopixel #1 = OFF
   np.write()
-  recv = bytes()
+  recv = bytes() # We will add bytes to recv as they come
   while uart.any()>0:
     recv += uart.read(1)
     time.sleep(0.01)
   txt=recv.decode('utf-8').strip().split('\n')
-  np[0] = (0, 0, 0)
+  # Split into lines for parsing
+  np[0] = (0, 0, 0) # Neopixel #0 = OFF
   np.write()
   print(txt)
   for line in txt:
+    # Regex recognizing the incoming text LEN sentence
     result = regexUARTlen.match(line)
     if result != None:
-      np[1] = (0, 0, 255)
+      np[1] = (0, 0, 255) # Neopixel #1 = Blue
       np.write()
       print(f'Incoming message at RSSI {result.group(2)}, SNR {result.group(3)}')
       oled.fill(0)
@@ -59,18 +75,22 @@ def handleIncoming():
       oled.text(f'RSSI {result.group(2)}, SNR {result.group(3)}', 1, 56)
       oled.show()
     else:
+      # Regex recognizing the incoming text content sentence
       result = regexUARTmsg.match(line)
       if result != None:
+        # dehex text
         msg = binascii.unhexlify(result.group(1))
         msg = msg.decode('utf-8').strip()
         print(msg)
         try:
+          # we are expecting JSON
           x = json.loads(msg)
           ln = 16
           for k in x.keys():
             oled.text(f'{k} {x[k]}', 1, ln)
             ln += 8
         except:
+          # If it's not JSON let's print it as is
           j = int(len(msg) / 16)
           ln = 16
           for i in range(j):
@@ -78,7 +98,7 @@ def handleIncoming():
             ln += 8
         oled.show()
         time.sleep(0.5)
-        np[1] = (0, 0, 0)
+        np[1] = (0, 0, 0) # Neopixel #1 = OFF
         np.write()
     
 oled.fill(0)
